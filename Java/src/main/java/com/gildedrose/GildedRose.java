2ad16984e14353c8bacdbec171483ca65b5dc622
Mantas Sutula <mantas.sut@gmail.com>
2018-02-06 20:15:37 +0000

    Replaced inverted conditionals and handle of special sulfuras case

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,54 +12,41 @@ class GildedRose {
 
     public void updateQuality() {
         items.stream().forEach(item -> {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                return;
+            }
+
+            if (item.name.equals("Aged Brie")) {
+                this.improveQuality(item);
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                this.improveQuality(item);
+                if (item.sellIn < 11) {
+                    this.improveQuality(item);
+                }
+                if (item.sellIn < 6) {
+                    this.improveQuality(item);
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                if (item.name.equals("Aged Brie")) {
+                    this.improveQuality(item);
+                } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    item.quality = 0;
+                } else if (item.quality > 0) {
+                    item.quality = item.quality - 1;
                 }
             }
         });
+    }
+
+    private void improveQuality(Item item) {
+        if (item.quality < 50) item.quality++;
     }
 }
