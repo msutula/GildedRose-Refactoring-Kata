a121775974ddebd9409e011d0d96a76342a4edd6
Mantas Sutula <mantas.sut@gmail.com>
2018-02-07 19:40:07 +0000

    Updated Gilded Rose to use quality logicalised items

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,54 +1,20 @@
 package com.gildedrose;
 
-import com.gildedrose.models.Item;
+import com.gildedrose.models.LogicalisedItem;
 
 import java.util.List;
 
 class GildedRose {
 
-    List<Item> items;
+    List<LogicalisedItem> logicalisedItems;
 
-    public GildedRose(List<Item> items) {
-        this.items = items;
+    public GildedRose(List<LogicalisedItem> logicalisedItems) {
+        this.logicalisedItems = logicalisedItems;
     }
 
     public void updateQuality() {
-        items.stream().forEach(item -> {
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                return;
-            }
-
-            if (item.name.equals("Aged Brie")) {
-                this.improveQuality(item);
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                this.improveQuality(item);
-                if (item.sellIn < 11) {
-                    this.improveQuality(item);
-                }
-                if (item.sellIn < 6) {
-                    this.improveQuality(item);
-                }
-            } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            }
-
-            item.sellIn = item.sellIn - 1;
-
-            if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
-                    this.improveQuality(item);
-                } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    item.quality = 0;
-                } else if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-            }
+        this.logicalisedItems.stream().forEach(localisedItem -> {
+            localisedItem.getQualityLogic().updateItem(localisedItem.getItem());
         });
-    }
-
-    private void improveQuality(Item item) {
-        if (item.quality < 50) item.quality++;
     }
 }
