7226670d7a9c3fb41b14a0e0e6ddb606f16ecbcc
Mantas Sutula <mantas.sut@gmail.com>
2018-02-07 19:40:07 +0000

    Removed multiple calls to increase/decrease quality

--- Java/src/main/java/com/gildedrose/models/AgedBrieLogic.java
+++ Java/src/main/java/com/gildedrose/models/AgedBrieLogic.java
@@ -4,8 +4,8 @@ public class AgedBrieLogic extends QualityLogic {
 
     @Override
     public void updateItem(Item item) {
-        this.increaseQuality(item);
+        this.increaseQuality(item, 1);
         this.decreaseSellIn(item);
-        if (item.sellIn < 0) this.increaseQuality(item);
+        if (item.sellIn < 0) this.increaseQuality(item, 1);
     }
 }
--- Java/src/main/java/com/gildedrose/models/ItemLogic.java
+++ Java/src/main/java/com/gildedrose/models/ItemLogic.java
@@ -4,8 +4,8 @@ public class ItemLogic extends QualityLogic {
 
     @Override
     public void updateItem(Item item) {
-        this.decreaseQuality(item);
+        this.decreaseQuality(item, 1);
         this.decreaseSellIn(item);
-        if (item.sellIn < 0) this.decreaseQuality(item);
+        if (item.sellIn < 0) this.decreaseQuality(item, 1);
     }
 }
