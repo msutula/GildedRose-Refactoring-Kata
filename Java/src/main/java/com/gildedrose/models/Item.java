0cdb0616044e86ef4ecd7b260c4e9839ada906dc
Mantas Sutula <mantas.sut@gmail.com>
2018-02-08 19:36:18 +0000

    Added spring rest data items service

--- Java/src/main/java/com/gildedrose/models/Item.java
+++ Java/src/main/java/com/gildedrose/models/Item.java
@@ -1,12 +1,27 @@
 package com.gildedrose.models;
 
+
+import javax.persistence.*;
+
+@Entity
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    // Public left to support the 1st part of the task, ideally would be private
+    @Column
     public String name;
 
+    @Column
     public int sellIn;
 
+    @Column
     public int quality;
+
+    public Item() {
+    }
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -14,8 +29,8 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
